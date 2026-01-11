0455577e99dc634483bf9497596b93c33ec230b3
Omar <omarelsayedmuhammad@gmail.com>
2026-01-11 08:17:13 +0200

    added serialize response method

--- src/protocol/Protocol.js
+++ src/protocol/Protocol.js
@@ -7,19 +7,22 @@ export default class Protocol {
     DEL: 3,
     LS: 4,
     RESPONSE_OK: 5,
-    RESPONSE_ERROR: 6,
+    RESPONSE_FAIL: 6,
+    RESPONSE_ERROR: 7,
   };
 
-  static #LIMITS = {
+  static #allowedResponseStatus = ["ok", "fail", "error"];
+
+  static #limits = {
     command: 1, // 1 Bytes
     key: 2, // 2 Bytes
     value: 4, // 4 Bytes
   };
 
   static #getAllocatedBuffer(key, value) {
-    let size = this.#LIMITS.command + this.#LIMITS.key + key.length;
+    let size = this.#limits.command + this.#limits.key + key.length;
 
-    if (value) size += this.#LIMITS.value + value.length;
+    if (value) size += this.#limits.value + value.length;
 
     return Buffer.alloc(size);
   }
@@ -27,7 +30,7 @@ export default class Protocol {
   static #writeSerializedCmd(buffer, cmd) {
     buffer.writeUint8(this.#CMDs[cmd], 0);
 
-    return this.#LIMITS.command; // the offset
+    return this.#limits.command; // the offset
   }
 
   static #writeSerializedKey(key, buffer, offset) {
@@ -35,7 +38,7 @@ export default class Protocol {
     const keyLen = key.length;
 
     buffer.writeUint16BE(keyLen, offset);
-    offset += this.#LIMITS.key;
+    offset += this.#limits.key;
 
     keyBuff.copy(buffer, offset);
     offset += keyLen;
@@ -56,7 +59,7 @@ export default class Protocol {
     offset = this.#writeSerializedKey(key, buffer, offset);
 
     buffer.writeUInt32BE(valueLen, offset);
-    offset += this.#LIMITS.value;
+    offset += this.#limits.value;
 
     valueBuff.copy(buffer, offset);
 
@@ -86,7 +89,7 @@ export default class Protocol {
     let offset = 0;
 
     // deserialize command
-    const type = buffer.readUint8(0);
+    const type = buffer.readUint8(offset);
 
     switch (type) {
       case 1:
@@ -102,12 +105,12 @@ export default class Protocol {
         throw new Error("Error: Unkown type");
     }
 
-    offset += this.#LIMITS.command;
+    offset += this.#limits.command;
 
     // Deserialize Key
     const keyBytes = buffer.readUint16BE(offset);
 
-    offset += this.#LIMITS.key; // start reading
+    offset += this.#limits.key; // start reading
     const keyBuf = buffer.subarray(offset, keyBytes + offset);
 
     payload.key = keyBuf.toString("utf-8");
@@ -118,12 +121,35 @@ export default class Protocol {
     if (payload.type !== "SET") return payload;
 
     const valueBytes = buffer.readUint32BE(offset);
-    offset += this.#LIMITS.value;
+    offset += this.#limits.value;
 
     const valueBuf = buffer.subarray(offset, valueBytes + offset);
 
     payload.value = valueBuf.toString("utf-8");
 
     return payload;
+  }
+
+  static serializeResponse(status, data) {
+    if (!status || !this.#allowedResponseStatus.includes(status))
+      throw new Error("Error: Status Unkown.");
+
+    const dataBuff = Buffer.from(JSON.stringify(data));
+    const statusBuff = Buffer.alloc(this.#limits.command);
+
+    switch (status) {
+      case "ok":
+        statusBuff.writeUint8(this.#CMDs.RESPONSE_OK);
+        break;
+      case "fail":
+        statusBuff.writeUint8(this.#CMDs.RESPONSE_FAIL);
+        break;
+      case "error":
+        statusBuff.writeUint8(this.#CMDs.RESPONSE_ERROR);
+        break;
+    }
+
+    return Buffer.concat([statusBuff, dataBuff]);
+    const jsonResponse = JSON.stringify(response);
   }
 }
