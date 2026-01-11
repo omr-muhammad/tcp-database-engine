2e2683073cefd745f938519639cbf29268eea024
Omar <omarelsayedmuhammad@gmail.com>
2026-01-11 23:58:34 +0200

    added message receiving and handled command execution

--- src/server/tcpServer.js
+++ src/server/tcpServer.js
@@ -1,5 +1,10 @@
 import net from "node:net";
 
+import Protocol from "../protocol/Protocol.js";
+import DiskStorage from "../storage/DiskStorage.js";
+
+const store = new DiskStorage();
+
 const pool = new Map();
 let id = 1;
 
@@ -14,6 +19,26 @@ function handleConnections(socket) {
   const clientId = id;
   console.log(`New connection with id: ${clientId}`);
 
+  let requestBuff = Buffer.alloc(0);
+  let messageSize;
+  socket.on("data", (chunk) => {
+    requestBuff = Buffer.concat([requestBuff, chunk]);
+
+    if (requestBuff.byteLength > 3) messageSize = requestBuff.readUint32BE(0);
+
+    if (messageSize && requestBuff.byteLength === messageSize) {
+      const messageBuff = requestBuff.subarray(4);
+
+      const request = Protocol.deserializeRequest(messageBuff);
+
+      const { status, data } = handleRequest(request);
+
+      const responseBuff = Protocol.serializeResponse(status, data);
+
+      socket.write(responseBuff);
+    }
+  });
+
   socket.on("end", () => {
     pool.delete(clientId);
     console.log(`Client with id: ${clientId} left!`);
@@ -21,4 +46,50 @@ function handleConnections(socket) {
 
   pool.set(clientId, socket);
   id++;
+}
+
+function handleRequest(req) {
+  if (req.type === "SET") {
+    try {
+      store.set(req.key, req.value);
+      return {
+        status: "ok",
+        data: {
+          message: `${req.key} successfully set to ${req.value}`,
+        },
+      };
+    } catch (error) {
+      if (!error.message) console.log("SET Error: ", error);
+
+      return {
+        status: error.message ? "fail" : "error",
+        data: {
+          message: error.message || "Unkown Error.",
+        },
+      };
+    }
+  } else if (req.type === "GET") {
+    const value = store.get(req.key);
+
+    return {
+      status: "ok",
+      data: {
+        value,
+      },
+    };
+  } else if (req.type === "DEL") {
+    store.delete(req.key);
+
+    return {
+      status: "ok",
+      data: { message: "Key is successfully deleted." },
+    };
+  } else {
+    return {
+      status: "fail",
+      data: {
+        message: `Invalid action type got ${req.type}`,
+      },
+    };
+  }
 }
