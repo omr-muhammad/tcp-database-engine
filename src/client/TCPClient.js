179512a8a39a8f11c41f403c089146a2b527ff8a
Omar <omarelsayedmuhammad@gmail.com>
2026-01-13 09:24:02 +0200

    add: impelemented connect/disconnect methods

--- src/client/TCPClient.js
+++ src/client/TCPClient.js
@@ -3,9 +3,18 @@ import net from "node:net";
 class TCPClient {
   #host;
   #port;
+  #clientSocket;
 
   constructor(host, port) {
     this.#host = host;
     this.#port = port;
+  }
+
+  connect() {
+    this.#clientSocket = net.connect({ host: this.#host, port: this.#port });
+  }
+
+  disconnect() {
+    this.#clientSocket.end();
   }
 }
