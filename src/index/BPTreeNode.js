187a4678837ae66900010d81ea394f67f85ee452
Omar <omarelsayedmuhammad@gmail.com>
2026-01-18 16:20:42 +0200

    add: replaced B-Tree with B+-Tree for better range queries while increasing insertion/deletion pain

---
+++ src/index/BPTreeNode.js
@@ -0,0 +1,41 @@
+export default class BPTreeNode {
+  constructor(maxKeys, isLeaf) {
+    this.keys = [];
+    this.children = [];
+    this.isLeaf = isLeaf;
+    this.maxKeys = maxKeys;
+    this.parent = null;
+
+    if (isLeaf) {
+      this.next = null;
+      this.pairs = [];
+    }
+  }
+
+  isFull() {
+    return this.keys.length >= this.maxKeys;
+  }
+
+  getIndexToInsert(key) {
+    let min = 0;
+    let max = this.keys.length - 1;
+
+    let mid;
+    while (min < max) {
+      mid = Math.floor((min + max) / 2);
+
+      if (key === this.keys[mid].key) return mid;
+      else if (key < this.keys[mid].key) max = mid - 1;
+      else min = mid + 1;
+    }
+
+    // min will be the insertion index if key is not exist
+    return min;
+  }
+
+  insertAt(idx, key, value) {
+    this.keys = this.keys.toSpliced(idx, 0, key);
+
+    if (this.isLeaf) this.pairs = this.pairs.toSpliced(idx, 0, { key, value });
+  }
+}
