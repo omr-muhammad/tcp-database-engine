00c6e3a01696d659dd056d281c3b3b79f1537ada
Omar <omarelsayedmuhammad@gmail.com>
2026-01-20 20:30:15 +0200

    add: handled read/write to disk

--- src/storage/DiskStorage.js
+++ src/storage/DiskStorage.js
@@ -1,72 +1,138 @@
 import fs from "node:fs/promises";
-import MemoryStore from "./MemoryStore.js";
 import path from "node:path";
 
-export default class DiskStore extends MemoryStore {
-  #filePath;
-  #tmpFileName = "tempDB.json";
+export default class DiskStore {
+  #dataPath;
+  #tmpFileName = "tempDB.db";
+  #maxDataBytes = 4.194e6;
+  #isInitialized = false;
 
   #getFileDir() {
-    const fileIdx = this.#filePath.lastIndexOf("/");
+    const fileIdx = this.#dataPath.lastIndexOf("/");
 
-    return this.#filePath.slice(0, fileIdx);
+    return this.#dataPath.slice(0, fileIdx);
   }
 
   async #ensureDataDir() {
-    await fs.mkdir(this.#getFileDir, { recursive: true });
+    await fs.mkdir(this.#getFileDir(), { recursive: true });
   }
 
-  constructor(filePath = "./data/db.json") {
-    super();
-    this.#filePath = filePath;
+  constructor(dataPath = "./data/data.db") {
+    this.#dataPath = dataPath;
   }
 
-  async initialize() {
+  // async initialize() {
+  //   try {
+  //     await this.load();
+  //   } catch (error) {
+  //     console.error("Error Loading DB: ", error);
+  //     // this._store = new Map();
+  //   }
+  // }
+
+  async writeValue(buffer) {
+    if (!this.#isInitialized) {
+      this.#isInitialized = true;
+      await this.#ensureDataDir();
+    }
+
+    if (!(buffer instanceof Buffer) || buffer.byteLength > this.#maxDataBytes)
+      throw new Error(
+        `Invalid data format expect buffer or data exceeded limit max ${this.#maxDataBytes}`,
+      );
+
+    const head = Buffer.alloc(4); // 4 Bytes
+    head.writeUint32BE(buffer.byteLength, 0);
+
+    let fileHandler;
     try {
-      await this.load();
-    } catch (error) {
-      console.error("Error Loading DB: ", error);
+      const fileHandler = await fs.open(this.#dataPath, "r+");
+      const fileStats = await fileHandler.stat();
+      const offset = fileStats.size;
 
-      this._store = new Map();
+      await fileHandler.writev([head, buffer], offset);
+
+      return offset;
+    } catch (err) {
+      console.error("Error Writing to file 💥", err);
+      return null;
+    } finally {
+      await fileHandler.close();
     }
   }
 
-  async flush() {
-    await this.#ensureDataDir();
-
-    const dataObject = Object.fromEntries(this._store);
-    const dataJson = JSON.stringify(dataObject);
-
-    const dbDir = this.#getFileDir();
-    const tempFilePath = path.resolve(dbDir, this.#tmpFileName);
-
-    // write to tmp first
-    await fs.writeFile(tempFilePath, dataJson, "utf-8");
-
-    // rename to override old data (atomic)
-    await fs.rename(tempFilePath, this.#filePath);
-  }
-
-  async load() {
+  async readValue(offset) {
+    let fileHandler;
     try {
-      const fileData = await fs.readFile(this.#filePath, "utf-8");
-      const parsedData = JSON.parse(fileData);
+      fileHandler = await fs.open(this.#dataPath, "r");
+      const stats = await fileHandler.stat();
 
-      this._store = new Map(Object.entries(parsedData));
-    } catch (error) {
-      if (error.code === "ENOENT") {
-        // When file not exits
-        this._store = new Map();
-        return;
-      }
+      if (offset > stats.size) throw new Error("Invalid range");
 
-      throw error;
+      const headBuff = Buffer.alloc(4);
+
+      await fileHandler.read({
+        buffer: headBuff,
+        offset: 0,
+        length: headBuff.byteLength,
+        position: offset,
+      });
+
+      const dataBytes = headBuff.readUint32BE(0);
+
+      const dataBuff = Buffer.alloc(dataBytes);
+
+      await fileHandler.read({
+        buffer: dataBuff,
+        offset: 0, // start place to write in buffer
+        length: dataBuff.byteLength, // number of bytes to read
+        position: offset + 4, // start place to read from file
+      });
+
+      return dataBuff;
+    } catch (err) {
+      console.error("Error Reading file 💥", err);
+    } finally {
+      await fileHandler.close();
     }
   }
+
+  // async flush() {
+  //   await this.#ensureDataDir();
+
+  //   const dataObject = Object.fromEntries(this._store);
+  //   const dataJson = JSON.stringify(dataObject);
+
+  //   const dbDir = this.#getFileDir();
+  //   const tempFilePath = path.resolve(dbDir, this.#tmpFileName);
+
+  //   // write to tmp first
+  //   await fs.writeFile(tempFilePath, dataJson, "utf-8");
+
+  //   // rename to override old data (atomic)
+  //   await fs.rename(tempFilePath, this.#dataPath);
+  // }
+
+  // async load() {
+  //   try {
+  //     const fileData = await fs.readFile(this.#dataPath, "utf-8");
+  //     const parsedData = JSON.parse(fileData);
+
+  //     this._store = new Map(Object.entries(parsedData));
+  //   } catch (error) {
+  //     if (error.code === "ENOENT") {
+  //       // When file not exits
+  //       this._store = new Map();
+  //       return;
+  //     }
+
+  //     throw error;
+  //   }
+  // }
 
   async clear() {
     try {
-      await fs.unlink(this.#filePath);
+      await fs.unlink(this.#dataPath);
     } catch (error) {
       console.log("ERROR Msg: ", error.message);
       console.log("ERROR: ", error);
